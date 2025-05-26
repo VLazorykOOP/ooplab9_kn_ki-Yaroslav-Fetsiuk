40443819849b05a259f3b87fc5ab2fa95b082bbc
Yaroslav-Fetsiuk <fetsiuk.yaroslav.clg@chnu.edu.ua>
2025-05-26 20:01:25 +0300

    Add files via upload

---
+++ Завдання №1/Завдання №1/Завдання №1.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <list>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+// Типи для вводу та виводу
+typedef istream_iterator<int> ptin;
+typedef ostream_iterator<int> ptout;
+
+// Функція для виведення списку з поясненням
+template<typename Iterator>
+void ShowLine(Iterator begin, Iterator end, const string& label) {
+    cout << label;
+    for (auto it = begin; it != end; ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+
+    setlocale(LC_ALL, "ukr");
+
+    // Зчитування списку з консолі
+    list<int> L((ptin(cin)), ptin());
+
+    // Вивід початкового списку
+    ShowLine(L.begin(), L.end(), "Список до обробки: ");
+
+    // Обхід у зворотному порядку
+    auto rfirst = L.rbegin();
+    auto rlast = L.rend();
+
+    // Знаходження першої (тобто останньої у звичайному порядку) пари однакових сусідів
+    auto rit = adjacent_find(rfirst, rlast);
+
+    if (rit != rlast) {
+        // Створюємо звичайний (прямий) ітератор з реверсного:
+        // rit.base() - вказує на елемент ПІСЛЯ rit
+        auto insert_pos = rit.base(); // позиція другого елемента з пари
+
+        // Вставляємо нове значення перед другим однаковим
+        L.insert(insert_pos, *rit);
+    }
+
+    // Вивід фінального списку
+    ShowLine(L.begin(), L.end(), "Список після обробки: ");
+
+    return 0;
+}
---
+++ Завдання №2/Завдання №2/Завдання №2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <iterator>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    typedef istream_iterator<int> ptin;
+    typedef ostream_iterator<int> ptout;
+
+    // Зчитування першого рядка (для множини A)
+    string lineA;
+    getline(cin, lineA);
+    istringstream issA(lineA);
+    multiset<int> A((ptin(issA)), (ptin()));
+
+    // Зчитування другого рядка (для множини B)
+    string lineB;
+    getline(cin, lineB);
+    istringstream issB(lineB);
+    multiset<int> B((ptin(issB)), (ptin()));
+
+    // Копіюємо A у вектор для обробки
+    vector<int> vecA(A.begin(), A.end());
+    sort(vecA.begin(), vecA.end());
+
+    // Перебираємо всі елементи з B і видаляємо їх по одному з vecA
+    for (int b : B) {
+        auto it = lower_bound(vecA.begin(), vecA.end(), b);
+        auto itEnd = upper_bound(vecA.begin(), vecA.end(), b);
+
+        if (it != itEnd) {
+            vecA.erase(it); // видаляємо одне входження
+        }
+    }
+
+    // Виводимо результат
+    copy(vecA.begin(), vecA.end(), ptout(cout, " "));
+    cout << endl;
+
+    return 0;
+}
